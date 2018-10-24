253bc23b8efd4e4bb946f95cd34cbffb59c071c4
Norbert Nemes <norbert@aymcommerce.com>
2018-10-24 11:38:52 +0400

    kickoff

---
+++ TestProjectForCITesting/TestProjectForCITesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProjectForCITesting
+//
+//  Created by Norbert Nemes on 24/10/18.
+//  Copyright © 2018 Norbert Nemes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
