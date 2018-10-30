6dbac07eabbc22fe922ef99d3d2cec228ebbbc74
Norbert Nemes <norbert@aymcommerce.com>
2018-10-30 14:38:17 +0400

    hockey app id fixed

--- TestProjectForCITesting/appcenter-post-build.sh
+++ TestProjectForCITesting/appcenter-post-build.sh
@@ -3,13 +3,14 @@
 echo "deploy to HockeyApp"
 
 HOCKEYAPP_API_TOKEN={"bcf30a6a2e74429db9f6209975435367"}
-HOCKEYAPP_APP_ID={"1d2efae813e84184a1736209867059cac"}
+HOCKEYAPP_APP_ID={"1d2efae813e84184a1736209867059ca"}
 
 # Example: Upload master branch app binary to HockeyApp using the API
 if [ "$APPCENTER_BRANCH" == "master" ];
 then
 curl \
 -F "status=2" \
+-F "notify=1" \
 -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/TestProjectForCITesting.ipa" \
 -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" \
 https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload
