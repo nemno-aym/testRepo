5e08ebda5165fb2456ea9053c26bba83b99000b1
Norbert Nemes <norbert@aymcommerce.com>
2018-10-30 14:57:35 +0400

    hardcode keys

--- TestProjectForCITesting/appcenter-post-build.sh
+++ TestProjectForCITesting/appcenter-post-build.sh
@@ -12,8 +12,8 @@ curl \
 -F "status=2" \
 -F "notify=1" \
 -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/TestProjectForCITesting.ipa" \
--H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" \
-https://rink.hockeyapp.net/api/2/apps/"$HOCKEYAPP_APP_ID"/app_versions/upload
+-H "X-HockeyAppToken: bcf30a6a2e74429db9f6209975435367" \
+https://rink.hockeyapp.net/api/2/apps/1d2efae813e84184a1736209867059ca/app_versions/upload
 else
 echo "Current branch is $APPCENTER_BRANCH"
 fi
